095822fd00a081299bf1281f551f787cfe7516fc
Laxman <laxmanmule13@gmail.com>
2022-05-11 10:22:36 +0530

    change name

--- src/HelloNewWorld.java
+++ src/HelloNewWorld.java
@@ -1,5 +1,6 @@
 public class HelloNewWorld {
     public static void main(String[] args) {
         System.out.println("Hello new world");
+        System.out.println("hii this is Lakhan mule");
     }
 }
